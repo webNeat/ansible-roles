9d85baa5e856d4bc629d8c5d49d5973860f2a09d
Amine Ben hammou <webneat@gmail.com>
2022-12-25 23:50:22 +0100

    Add mysql

---
+++ create-mysql-database/README.md
@@ -0,0 +1,9 @@
+# Requirements
+
+- `install-mysql`
+
+# Variables
+
+- `db_name`: the database name.
+- `db_user_name`: the database owner name.
+- `db_user_pass`: the database owner password.
--- deploy-site/templates/deploy.sh
+++ deploy-site/templates/deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 export PATH="$PATH:$HOME/.npm/bin"
 
-cd ~/sites/{{site_name}}/code
+cd {{home_dir}}/sites/{{site_name}}/code
 git checkout -- .
 git checkout {{repo_branch}}
 git pull origin {{repo_branch}}
